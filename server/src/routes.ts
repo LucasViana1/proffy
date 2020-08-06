18da7b1fa4b625d1dcdeb110fa4cedf858121e99
Lucas Cunha <lucasviana112@gmail.com>
2020-08-06 08:51:57 -0300

    routes file (create and list classes)

---
+++ server/src/routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+
+const classesController = new ClassesController();
+
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+export default routes;
