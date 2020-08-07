f19e79dbd2097d4ecefe727cac3bd17d7a918fa4
Lucas Cunha <lucasviana112@gmail.com>
2020-08-07 20:39:43 -0300

    statement of the teacher's details and integration to the API connection creation route

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,52 @@
-import React from "react";
-import "./styles.css";
+import React, { useCallback } from 'react';
+import './styles.css';
 
-import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = useCallback(() => {
+    api.post('connections', { user_id: teacher.id });
+  }, [teacher.id]);
+
   return (
-    <article className="teacher-item">
+    <article className='teacher-item'>
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/50250861?s=460&u=ae06093f9ff8f002958cc0b5098d7103597c2006&v=4"
-          alt="avatar"
-        />
+        <img src={teacher.avatar} alt='avatar' />
         <div>
-          <strong>Lucas Viana</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusias em tecnologia, busca soluções através da resolução de problemas
-        por meio de código
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 20,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
-          <img src={whatsappIcon} alt="WhatsApp" />
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target='__blank'
+        >
+          <img src={whatsappIcon} alt='WhatsApp' />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
