08c0b3c91f5883b7bb46257d25dd56503f31733a
Lucas Cunha <lucasviana112@gmail.com>
2020-08-07 09:24:30 -0300

    accept description props in the pageHeader component

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,27 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./styles.css";
+import './styles.css';
 
-import logoImg from "../../assets/images/logo.svg";
-import backIcon from "../../assets/images/icons/back.svg";
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  description,
+  children,
+}) => {
   return (
-    <header className="page-header">
-      <div className="top-bar-container">
-        <Link to="/">
-          <img src={backIcon} alt="" />
+    <header className='page-header'>
+      <div className='top-bar-container'>
+        <Link to='/'>
+          <img src={backIcon} alt='' />
         </Link>
-        <img src={logoImg} alt="Logo Proffy" />
+        <img src={logoImg} alt='Logo Proffy' />
       </div>
 
-      <div className="header-content">
+      <div className='header-content'>
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
+
         {children}
       </div>
     </header>
